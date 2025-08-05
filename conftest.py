995ae499c0efccaf6e93952c7dd36b8b4528002a
Lovish Goel <lovishgoel94@gmail.com>
2025-08-05 14:24:09 +0200

    Hook added for capturing screenshots if test is failed in conftest.py

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from playwright.sync_api import sync_playwright
+import os
 @pytest.fixture(scope="function")
 def page():
     with sync_playwright() as p:
@@ -10,4 +11,23 @@ def page():
         yield page
         context.close()
         browser.close()
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    # This hook runs after each test
+    outcome = yield
+    rep = outcome.get_result()
+    # Only act on test failures
+    if rep.when == "call" and rep.failed:
+        page = item.funcargs.get("page", None)
+        if page:
+            page_name = item.name
+            screenshot_path = os.path.join("screenshots", f"{page_name}.png")
+            page.screenshot(path = screenshot_path)
+            print(f"\n screenshot saved to {screenshot_path}")
+
+
+
+
+
 
